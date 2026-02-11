cb93aa0c99fdae53c7cc67de380ff2724484d401
waghvaishnav <waghvaishnav4@gmail.com>
2026-02-11 23:36:07 +0530

    updated file name

--- prediction_helper.py
+++ prediction_helper.py
@@ -3,9 +3,9 @@
 import pandas as pd
 import joblib
 
-model_young = joblib.load("artifacts/model_young.joblib")
+model_young = joblib.load("artifacts/model_young_gr.joblib")
 model_rest = joblib.load("artifacts/model_rest.joblib")
-scaler_young = joblib.load("artifacts/scaler_young.joblib")
+scaler_young = joblib.load("artifacts/scaler_young_gr.joblib")
 scaler_rest = joblib.load("artifacts/scaler_rest.joblib")
 
 def calculate_normalized_risk(medical_history):
